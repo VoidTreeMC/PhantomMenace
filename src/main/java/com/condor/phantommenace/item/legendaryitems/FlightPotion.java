8267818be5a8a95036ebad97d09d171a4cf985e9
iron-condor <christiandloftis@gmail.com>
2021-11-01 12:18:17 -0400

    If mobs deal a certain amount of damage to you while flying with a flight potion, it turns the flight effect off for a few seconds and gives you slow fall

--- src/main/java/com/condor/phantommenace/item/legendaryitems/FlightPotion.java
+++ src/main/java/com/condor/phantommenace/item/legendaryitems/FlightPotion.java
@@ -37,11 +37,17 @@ public class FlightPotion extends CustomItem {
   // 20 minutes
   private static final long DURATION_TICKS = 20 * 20 * 60;
   private static final long DURATION_MS = DURATION_TICKS / 20 * 1000;
+  public static final long COOLDOWN_TIME_IN_SECONDS = 10;
+  public static final long COOLDOWN_TIME = 20 * COOLDOWN_TIME_IN_SECONDS;
+  // The maximum amount of damage a player can take before their flight potion goes on cooldown
+  private static final int MAX_DMG = 20;
 
   public static final String METADATA_KEY = "affectedByFlightPotion";
 
   // Keeps track of when the flight potion should wear off for each user
   private static TreeMap<UUID, Long> userMap = new TreeMap<>();
+  // Keeps track of how much damage the user has taken since their last reset
+  private static TreeMap<UUID, Double> damageMap = new TreeMap<>();
 
   static {
     loreList.add("Flight Potion");
@@ -53,6 +59,7 @@ public class FlightPotion extends CustomItem {
     loreList.add("");
 
     triggerList.add(PlayerItemConsumeEvent.class);
+    triggerList.add(EntityDamageByEntityEvent.class);
   }
 
   public FlightPotion() {
@@ -79,29 +86,79 @@ public class FlightPotion extends CustomItem {
       if (type == CustomItemType.FLIGHT_POTION) {
         ret = true;
       }
+    } else if (event instanceof EntityDamageByEntityEvent) {
+      EntityDamageByEntityEvent edbee = (EntityDamageByEntityEvent) event;
+      if (edbee.getEntity() instanceof Player) {
+        ret = isAffectedByFlightPotion((Player) edbee.getEntity());
+      }
     }
     return ret;
   }
 
   public void execute(Event event) {
-    PlayerItemConsumeEvent pice = (PlayerItemConsumeEvent) event;
-    Player player = pice.getPlayer();
-    player.setAllowFlight(true);
-    player.setMetadata(METADATA_KEY, new FixedMetadataValue(PhantomMain.getPlugin(), true));
-    long currTime = System.currentTimeMillis();
-    // Bukkit.getLogger().info("Drank at " + currTime);
-    long wearOffTime = currTime + DURATION_MS;
-    // Bukkit.getLogger().info("Defaulting wear-off time to " + wearOffTime);
-    if (userMap.containsKey(player.getUniqueId())) {
-      wearOffTime = userMap.get(player.getUniqueId()) + DURATION_MS;
-      player.sendMessage(ChatColor.AQUA + "Your flight period has been extended.");
-      // Bukkit.getLogger().info("Bumping it up to " + wearOffTime + " ( " + userMap.get(player.getUniqueId()) + " + " + DURATION_MS + " )");
-    } else {
-      player.sendMessage(ChatColor.AQUA + "The ground loses its grip on you.");
+    if (event instanceof PlayerItemConsumeEvent) {
+      PlayerItemConsumeEvent pice = (PlayerItemConsumeEvent) event;
+      Player player = pice.getPlayer();
+      player.setAllowFlight(true);
+      player.setMetadata(METADATA_KEY, new FixedMetadataValue(PhantomMain.getPlugin(), true));
+      long currTime = System.currentTimeMillis();
+      // Bukkit.getLogger().info("Drank at " + currTime);
+      long wearOffTime = currTime + DURATION_MS;
+      // Bukkit.getLogger().info("Defaulting wear-off time to " + wearOffTime);
+      if (userMap.containsKey(player.getUniqueId())) {
+        wearOffTime = userMap.get(player.getUniqueId()) + DURATION_MS;
+        player.sendMessage(ChatColor.AQUA + "Your flight period has been extended.");
+        // Bukkit.getLogger().info("Bumping it up to " + wearOffTime + " ( " + userMap.get(player.getUniqueId()) + " + " + DURATION_MS + " )");
+      } else {
+        player.sendMessage(ChatColor.AQUA + "The ground loses its grip on you.");
+      }
+      // Bukkit.getLogger().info("Wear off time: " + wearOffTime);
+      (new RemoveFlightEffect(player, wearOffTime, true)).runTaskLaterAsynchronously(PhantomMain.getPlugin(), (wearOffTime - currTime) / 1000 * 20);
+      userMap.put(player.getUniqueId(), wearOffTime);
+    } else if (event instanceof EntityDamageByEntityEvent) {
+      EntityDamageByEntityEvent edbee = (EntityDamageByEntityEvent) event;
+      Player player = (Player) edbee.getEntity();
+      double finalDam = 0.0;
+      boolean over = false;
+      if (damageMap.containsKey(player.getUniqueId())) {
+        finalDam = damageMap.get(player.getUniqueId());
+      }
+      finalDam += edbee.getFinalDamage();
+      if (finalDam >= MAX_DMG) {
+        finalDam = 0;
+        over = true;
+      }
+      damageMap.put(player.getUniqueId(), finalDam);
+      // If they've gone over the max amount of damage they can take
+      if (over) {
+        (new RemoveFlightEffect(player, System.currentTimeMillis(), false)).runTaskAsynchronously(PhantomMain.getPlugin());
+        Bukkit.getScheduler().runTaskLater(PhantomMain.getPlugin(), new Runnable() {
+    			@Override
+    			public void run() {
+            if (isAffectedByFlightPotion(player)) {
+              player.setAllowFlight(true);
+            }
+    	    }
+        }, COOLDOWN_TIME);
+      }
     }
-    // Bukkit.getLogger().info("Wear off time: " + wearOffTime);
-    (new RemoveFlightEffect(player, wearOffTime)).runTaskLaterAsynchronously(PhantomMain.getPlugin(), (wearOffTime - currTime) / 1000 * 20);
-    userMap.put(player.getUniqueId(), wearOffTime);
+  }
+
+  /**
+   * Returns true if the player is affected by a flight potion
+   * @param  player The player whose flight condition is to be assessed
+   * @return        True if the player can fly due to a flight potion, false otherwise
+   */
+  public static boolean isAffectedByFlightPotion(Player player) {
+    UUID uuid = player.getUniqueId();
+    if (!userMap.containsKey(uuid)) {
+      return false;
+    }
+    long now = System.currentTimeMillis();
+    if (now <= userMap.get(uuid)) {
+      return true;
+    }
+    return false;
   }
 
   public static boolean isOutdated(UUID uuid, long time /* no see */) {
--- src/main/java/com/condor/phantommenace/runnable/RemoveFlightEffect.java
+++ src/main/java/com/condor/phantommenace/runnable/RemoveFlightEffect.java
@@ -28,17 +28,24 @@ public class RemoveFlightEffect extends BukkitRunnable {
   // The time that the player drank the potion
   long finishTime;
 
+  // Whether or not we should check if this order is outdated
+  boolean checkTimes;
+
   private static final int SLOW_FALL_DURATION = 20 * 30;
+
+  private static final PotionEffect slowFall = new PotionEffect(PotionEffectType.SLOW_FALLING, SLOW_FALL_DURATION, 0, true, false, false);
+  private static final PotionEffect shortSlowFall = new PotionEffect(PotionEffectType.SLOW_FALLING, (int) FlightPotion.COOLDOWN_TIME, 0, true, false, false);
 
   /**
    * Removes the flight ability from the player
    * IMPORTANT: MUST BE RUN ASYNCHRONOUSLY
    * @param player  The player whose flight ability is to be removed
    */
-	public RemoveFlightEffect(Player player, long finishTime) {
+	public RemoveFlightEffect(Player player, long finishTime, boolean checkTimes) {
 		this.plugin = PhantomMain.getPlugin();
     this.finishTime = finishTime;
 		this.player = player;
+    this.checkTimes = checkTimes;
 	}
 
 	/**
@@ -57,38 +64,38 @@ public class RemoveFlightEffect extends BukkitRunnable {
       return;
     }
 
-    if (FlightPotion.isOutdated(this.player.getUniqueId(), this.finishTime)) {
+    if (checkTimes && FlightPotion.isOutdated(this.player.getUniqueId(), this.finishTime)) {
       return;
     }
 
     // Bukkit.getLogger().info("Sending message at: " + System.currentTimeMillis());
-    this.player.sendMessage(ChatColor.RED + "The ground is pulling you down. Now would be a good time to land.");
+    if (checkTimes) {
+      this.player.sendMessage(ChatColor.RED + "The ground is pulling you down. Now would be a good time to land.");
 
-    try {
-      Thread.sleep(1000 * 10);
-    } catch (InterruptedException e) {
-      // Do nothing
+      try {
+        Thread.sleep(1000 * 10);
+      } catch (InterruptedException e) {
+        // Do nothing
+      }
+
+      if (FlightPotion.isOutdated(this.player.getUniqueId(), this.finishTime)) {
+        return;
+      }
+      this.player.sendMessage(ChatColor.AQUA + "You feel heavier.");
+      this.player.removeMetadata(FlightPotion.METADATA_KEY, this.plugin);
+      FlightPotion.removeTimestamp(player.getUniqueId(), finishTime);
     }
 
-    if (FlightPotion.isOutdated(this.player.getUniqueId(), this.finishTime)) {
-      return;
-    }
-
-    // Bukkit.getLogger().info("Sending message at: " + System.currentTimeMillis());
-    this.player.sendMessage(ChatColor.AQUA + "You feel heavier.");
     this.player.setAllowFlight(false);
-    this.player.removeMetadata(FlightPotion.METADATA_KEY, this.plugin);
-    FlightPotion.removeTimestamp(player.getUniqueId(), finishTime);
 
-    PotionEffect slowFall = new PotionEffect(PotionEffectType.SLOW_FALLING, SLOW_FALL_DURATION, 0, true, false, false);
+    PotionEffect effect = (checkTimes) ? slowFall : shortSlowFall;
 
     Bukkit.getScheduler().runTask(PhantomMain.getPlugin(), new Runnable() {
 			@Override
 			public void run() {
-        player.addPotionEffect(slowFall);
+        player.addPotionEffect(effect);
 	    }
     });
-
 
     this.cancel();
 	}
